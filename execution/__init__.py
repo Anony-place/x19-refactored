534b6964fd1356ec32c6796e440c8bd2309cc56a
Anony-place <Anonyhelps.su@gmail.com>
2026-09-10 19:14:38 +0530

    fix: expose integrity-aware security memory

--- execution/__init__.py
+++ execution/__init__.py
@@ -6,21 +6,11 @@ from execution.policy_engine import ExecutionPolicy, PolicyEngine, policy_from_c
 from execution.sandbox import SandboxExecutor, SandboxPolicy
 from execution.memory_kernel import MemoryKernel, MemorySnapshot, MemoryItem
 from execution.security_memory_v2 import SecurityMemory, SecurityMemorySnapshot
+from execution.security_memory_v3 import SecurityMemory as SecurityMemoryV3, SecurityMemorySnapshot as SecurityMemorySnapshotV3
 
 __all__ = [
-    "CommandGateway",
-    "GatewayExecutorAdapter",
-    "CommandRequest",
-    "CommandResult",
-    "ExecutionPolicy",
-    "PolicyEngine",
-    "PolicyVerdict",
-    "policy_from_config",
-    "SandboxExecutor",
-    "SandboxPolicy",
-    "MemoryKernel",
-    "MemorySnapshot",
-    "MemoryItem",
-    "SecurityMemory",
-    "SecurityMemorySnapshot",
+    "CommandGateway", "GatewayExecutorAdapter", "CommandRequest", "CommandResult",
+    "ExecutionPolicy", "PolicyEngine", "PolicyVerdict", "policy_from_config",
+    "SandboxExecutor", "SandboxPolicy", "MemoryKernel", "MemorySnapshot", "MemoryItem",
+    "SecurityMemory", "SecurityMemorySnapshot", "SecurityMemoryV3", "SecurityMemorySnapshotV3",
 ]
